1ddf786c57ee233a08d889fffbe707e8a0224620
Alexander Wilkinson <awrdp@comcast.net>
2021-08-18 00:13:34 -0700

    Resizeable window

--- IconFoeCreator/Form1.Designer.cs
+++ IconFoeCreator/Form1.Designer.cs
@@ -46,13 +46,15 @@ namespace IconFoeCreator
             this.richTextBox_SetupRules = new System.Windows.Forms.RichTextBox();
             this.button_copySetup = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label_Faction
             // 
             this.label_Faction.AutoSize = true;
             this.label_Faction.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_Faction.Location = new System.Drawing.Point(43, 51);
+            this.label_Faction.Location = new System.Drawing.Point(43, 45);
             this.label_Faction.Name = "label_Faction";
             this.label_Faction.Size = new System.Drawing.Size(123, 37);
             this.label_Faction.TabIndex = 0;
@@ -63,9 +65,9 @@ namespace IconFoeCreator
             this.comboBox_Faction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_Faction.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_Faction.FormattingEnabled = true;
-            this.comboBox_Faction.Location = new System.Drawing.Point(50, 190);
+            this.comboBox_Faction.Location = new System.Drawing.Point(47, 185);
             this.comboBox_Faction.Name = "comboBox_Faction";
-            this.comboBox_Faction.Size = new System.Drawing.Size(394, 45);
+            this.comboBox_Faction.Size = new System.Drawing.Size(360, 45);
             this.comboBox_Faction.TabIndex = 1;
             // 
             // comboBox_Job
@@ -73,16 +75,16 @@ namespace IconFoeCreator
             this.comboBox_Job.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_Job.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_Job.FormattingEnabled = true;
-            this.comboBox_Job.Location = new System.Drawing.Point(50, 401);
+            this.comboBox_Job.Location = new System.Drawing.Point(47, 400);
             this.comboBox_Job.Name = "comboBox_Job";
-            this.comboBox_Job.Size = new System.Drawing.Size(394, 45);
+            this.comboBox_Job.Size = new System.Drawing.Size(360, 45);
             this.comboBox_Job.TabIndex = 3;
             // 
             // label_Class
             // 
             this.label_Class.AutoSize = true;
             this.label_Class.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_Class.Location = new System.Drawing.Point(43, 264);
+            this.label_Class.Location = new System.Drawing.Point(40, 260);
             this.label_Class.Name = "label_Class";
             this.label_Class.Size = new System.Drawing.Size(97, 37);
             this.label_Class.TabIndex = 2;
@@ -93,7 +95,7 @@ namespace IconFoeCreator
             this.comboBox_Chapter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_Chapter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_Chapter.FormattingEnabled = true;
-            this.comboBox_Chapter.Location = new System.Drawing.Point(50, 518);
+            this.comboBox_Chapter.Location = new System.Drawing.Point(47, 515);
             this.comboBox_Chapter.Name = "comboBox_Chapter";
             this.comboBox_Chapter.Size = new System.Drawing.Size(77, 45);
             this.comboBox_Chapter.TabIndex = 5;
@@ -102,7 +104,7 @@ namespace IconFoeCreator
             // 
             this.label_Chapter.AutoSize = true;
             this.label_Chapter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_Chapter.Location = new System.Drawing.Point(43, 478);
+            this.label_Chapter.Location = new System.Drawing.Point(40, 475);
             this.label_Chapter.Name = "label_Chapter";
             this.label_Chapter.Size = new System.Drawing.Size(131, 37);
             this.label_Chapter.TabIndex = 4;
@@ -110,20 +112,24 @@ namespace IconFoeCreator
             // 
             // richTextBox_Description
             // 
+            this.richTextBox_Description.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox_Description.Location = new System.Drawing.Point(490, 45);
+            this.richTextBox_Description.Location = new System.Drawing.Point(3, 3);
             this.richTextBox_Description.Name = "richTextBox_Description";
             this.richTextBox_Description.ReadOnly = true;
             this.richTextBox_Description.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox_Description.Size = new System.Drawing.Size(643, 793);
+            this.richTextBox_Description.Size = new System.Drawing.Size(666, 721);
             this.richTextBox_Description.TabIndex = 7;
             this.richTextBox_Description.Text = "";
             // 
             // checkBox_Damage
             // 
+            this.checkBox_Damage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox_Damage.AutoSize = true;
             this.checkBox_Damage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox_Damage.Location = new System.Drawing.Point(50, 800);
+            this.checkBox_Damage.Location = new System.Drawing.Point(47, 737);
             this.checkBox_Damage.Name = "checkBox_Damage";
             this.checkBox_Damage.Size = new System.Drawing.Size(260, 36);
             this.checkBox_Damage.TabIndex = 8;
@@ -133,8 +139,9 @@ namespace IconFoeCreator
             // 
             // button_copyDescription
             // 
+            this.button_copyDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button_copyDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_copyDescription.Location = new System.Drawing.Point(490, 844);
+            this.button_copyDescription.Location = new System.Drawing.Point(3, 731);
             this.button_copyDescription.Name = "button_copyDescription";
             this.button_copyDescription.Size = new System.Drawing.Size(260, 53);
             this.button_copyDescription.TabIndex = 9;
@@ -147,16 +154,16 @@ namespace IconFoeCreator
             this.comboBox_FactionGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_FactionGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_FactionGroup.FormattingEnabled = true;
-            this.comboBox_FactionGroup.Location = new System.Drawing.Point(50, 91);
+            this.comboBox_FactionGroup.Location = new System.Drawing.Point(47, 85);
             this.comboBox_FactionGroup.Name = "comboBox_FactionGroup";
-            this.comboBox_FactionGroup.Size = new System.Drawing.Size(394, 45);
+            this.comboBox_FactionGroup.Size = new System.Drawing.Size(360, 45);
             this.comboBox_FactionGroup.TabIndex = 11;
             // 
             // label_FactionTemplates
             // 
             this.label_FactionTemplates.AutoSize = true;
             this.label_FactionTemplates.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_FactionTemplates.Location = new System.Drawing.Point(43, 150);
+            this.label_FactionTemplates.Location = new System.Drawing.Point(40, 145);
             this.label_FactionTemplates.Name = "label_FactionTemplates";
             this.label_FactionTemplates.Size = new System.Drawing.Size(150, 37);
             this.label_FactionTemplates.TabIndex = 10;
@@ -167,16 +174,16 @@ namespace IconFoeCreator
             this.comboBox_JobGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_JobGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_JobGroup.FormattingEnabled = true;
-            this.comboBox_JobGroup.Location = new System.Drawing.Point(50, 304);
+            this.comboBox_JobGroup.Location = new System.Drawing.Point(47, 300);
             this.comboBox_JobGroup.Name = "comboBox_JobGroup";
-            this.comboBox_JobGroup.Size = new System.Drawing.Size(394, 45);
+            this.comboBox_JobGroup.Size = new System.Drawing.Size(360, 45);
             this.comboBox_JobGroup.TabIndex = 13;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(43, 361);
+            this.label1.Location = new System.Drawing.Point(40, 360);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 37);
             this.label1.TabIndex = 12;
@@ -184,9 +191,10 @@ namespace IconFoeCreator
             // 
             // checkBox_NonessentialTraits
             // 
+            this.checkBox_NonessentialTraits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.checkBox_NonessentialTraits.AutoSize = true;
             this.checkBox_NonessentialTraits.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox_NonessentialTraits.Location = new System.Drawing.Point(50, 758);
+            this.checkBox_NonessentialTraits.Location = new System.Drawing.Point(47, 695);
             this.checkBox_NonessentialTraits.Name = "checkBox_NonessentialTraits";
             this.checkBox_NonessentialTraits.Size = new System.Drawing.Size(363, 36);
             this.checkBox_NonessentialTraits.TabIndex = 14;
@@ -196,19 +204,23 @@ namespace IconFoeCreator
             // 
             // richTextBox_SetupRules
             // 
+            this.richTextBox_SetupRules.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox_SetupRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox_SetupRules.Location = new System.Drawing.Point(1166, 45);
+            this.richTextBox_SetupRules.Location = new System.Drawing.Point(675, 3);
             this.richTextBox_SetupRules.Name = "richTextBox_SetupRules";
             this.richTextBox_SetupRules.ReadOnly = true;
             this.richTextBox_SetupRules.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox_SetupRules.Size = new System.Drawing.Size(469, 793);
+            this.richTextBox_SetupRules.Size = new System.Drawing.Size(442, 721);
             this.richTextBox_SetupRules.TabIndex = 15;
             this.richTextBox_SetupRules.Text = "";
             // 
             // button_copySetup
             // 
+            this.button_copySetup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button_copySetup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_copySetup.Location = new System.Drawing.Point(1166, 844);
+            this.button_copySetup.Location = new System.Drawing.Point(675, 731);
             this.button_copySetup.Name = "button_copySetup";
             this.button_copySetup.Size = new System.Drawing.Size(260, 53);
             this.button_copySetup.TabIndex = 16;
@@ -218,8 +230,9 @@ namespace IconFoeCreator
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(50, 844);
+            this.button1.Location = new System.Drawing.Point(47, 779);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(260, 53);
             this.button1.TabIndex = 17;
@@ -227,30 +240,48 @@ namespace IconFoeCreator
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.ExportAsJsonClicked);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.button_copyDescription, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox_SetupRules, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button_copySetup, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox_Description, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(435, 45);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1120, 787);
+            this.tableLayoutPanel1.TabIndex = 18;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1659, 919);
+            this.ClientSize = new System.Drawing.Size(1578, 844);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button_copySetup);
-            this.Controls.Add(this.richTextBox_SetupRules);
             this.Controls.Add(this.checkBox_NonessentialTraits);
             this.Controls.Add(this.comboBox_JobGroup);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox_FactionGroup);
             this.Controls.Add(this.label_FactionTemplates);
-            this.Controls.Add(this.button_copyDescription);
             this.Controls.Add(this.checkBox_Damage);
-            this.Controls.Add(this.richTextBox_Description);
             this.Controls.Add(this.comboBox_Chapter);
             this.Controls.Add(this.label_Chapter);
             this.Controls.Add(this.comboBox_Job);
             this.Controls.Add(this.label_Class);
             this.Controls.Add(this.comboBox_Faction);
             this.Controls.Add(this.label_Faction);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Icon Foe Creator";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -275,6 +306,7 @@ namespace IconFoeCreator
         private System.Windows.Forms.RichTextBox richTextBox_SetupRules;
         private System.Windows.Forms.Button button_copySetup;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
