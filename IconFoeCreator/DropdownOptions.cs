57ecca64b2739c4e9de3877961cbf00e17636f25
Alexander Wilkinson <awrdp@comcast.net>
2022-07-15 12:55:48 -0700

    Update to use Trait Library and added type var to Statistics

--- IconFoeCreator/DropdownOptions.cs
+++ IconFoeCreator/DropdownOptions.cs
@@ -15,27 +15,18 @@ namespace IconFoeCreator
         public static readonly SolidColorBrush DEFAULT_BRUSH = Brushes.White;
 
         public List<ComboBoxItem> Factions;
-        public List<ComboBoxItem> Templates;
-        public List<ComboBoxItem> UniqueFoes;
-        public List<ComboBoxItem> Specials;
         public List<ComboBoxItem> Classes;
-        public List<ComboBoxItem> Jobs;
+        public List<ComboBoxItem> Foes;
 
         public void ConvertToDropdownItems(StatisticBuilder statBuilder)
         {
             Factions = new List<ComboBoxItem>();
-            Templates = new List<ComboBoxItem>();
-            UniqueFoes = new List<ComboBoxItem>();
-            Specials = new List<ComboBoxItem>();
             Classes = new List<ComboBoxItem>();
-            Jobs = new List<ComboBoxItem>();
+            Foes = new List<ComboBoxItem>();
 
-            ConvertGroupsToComboBoxItems(statBuilder.Factions, Factions);
-            ConvertStatisticsToComboBoxItems(statBuilder.Templates, Templates);
-            ConvertStatisticsToComboBoxItems(statBuilder.UniqueFoes, UniqueFoes);
-            ConvertStatisticsToComboBoxItems(statBuilder.Specials, Specials);
-            ConvertGroupsToComboBoxItems(statBuilder.Classes, Classes);
-            ConvertStatisticsToComboBoxItems(statBuilder.Jobs, Jobs);
+            ConvertStringsToComboBoxItems(statBuilder.Factions, Factions);
+            ConvertStringsToComboBoxItems(statBuilder.Classes, Classes);
+            ConvertStatisticsToComboBoxItems(statBuilder.Foes, Foes);
         }
 
         private void ConvertStatisticsToComboBoxItems(List<Statistics> stats, List<ComboBoxItem> output)
@@ -44,21 +35,13 @@ namespace IconFoeCreator
             {
                 string className = String.Empty;
                 Statistics statConverted = (Statistics)(object)stat;
-                if (!String.IsNullOrEmpty(statConverted.UsesClass))
+                if (!String.IsNullOrEmpty(statConverted.Class))
                 {
-                    className = statConverted.UsesClass;
+                    className = statConverted.Class;
                 }
                 else if (statConverted.IsMob.GetValueOrDefault(false))
                 {
                     className = StatisticBuilder.MOB;
-                }
-                else if (statConverted.Type.ToLower() == StatisticBuilder.TYPE_TEMPLATE && !statConverted.IsBasicTemplate)
-                {
-                    className = MainWindow.UNIQUE_CLASS;
-                }
-                else if (!String.IsNullOrEmpty(statConverted.Group))
-                {
-                    className = statConverted.Group;
                 }
 
                 // Change color based on class
@@ -77,12 +60,12 @@ namespace IconFoeCreator
             }
         }
 
-        private void ConvertGroupsToComboBoxItems(List<Group> names, List<ComboBoxItem> output)
+        private void ConvertStringsToComboBoxItems(List<string> names, List<ComboBoxItem> output)
         {
-            foreach (Group name in names)
+            foreach (string name in names)
             {
-                Brush backgroundBrush = ThemeColors.GetGradientClassColor(name.Name) ?? Brushes.White;
-                Brush borderBrush = ThemeColors.GetLinearClassColor(name.Name) ?? Brushes.LightGray;
+                Brush backgroundBrush = ThemeColors.GetGradientClassColor(name) ?? Brushes.White;
+                Brush borderBrush = ThemeColors.GetLinearClassColor(name) ?? Brushes.LightGray;
 
                 ComboBoxItem item = new ComboBoxItem()
                 {
